f625ab5bd6888e098e8db6bf7646ddf31fec6f37
Mayur-Sathe <sathemayur29@gmail.com>
2026-07-29 13:50:48 +0530

    Add ad script

--- inc/footer.php
+++ inc/footer.php
@@ -64,5 +64,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <script src="/assets/js/custom.js?v=<?= SITE_VERSION ?>" defer></script>
+    <script src="https://pl30587796.effectivecpmnetwork.com/d4/66/76/d46676a3db322a1615dcfd78193ce4d0.js"></script>
 </body>
 </html>
